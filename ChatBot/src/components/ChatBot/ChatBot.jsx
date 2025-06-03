0b0e0b1836ecb8180a249224ae817db8738e6556
AlexTGoCreative <tlbalexandru@yahoo.com>
2025-06-03 18:46:35 +0300

    ChatBot v2.1

--- ChatBot/src/components/ChatBot/ChatBot.jsx
+++ ChatBot/src/components/ChatBot/ChatBot.jsx
@@ -346,38 +346,56 @@ const Chatbot = ({ Data, onSelectHistory }) => {
 
       <div className="chatbot-popup">
         <div className="chat-header">
-          <div className="header-info">
-            <ChatbotIcon
-              onClick={() => {
-                setShowChatHistoryDropdown((prev) => !prev);
-                setShowScanDropdown(false);
-              }}
-            />
-            <h2 className="logo-text">Ozzy</h2>
-          </div>
-          <div className="header-buttons">
-            <button
-              onClick={() => {
-                setShowScanDropdown((prev) => !prev);
-                setShowChatHistoryDropdown(false);
-              }}
-              className="material-symbols-rounded"
-            >
-              schedule
-            </button>
-            <button onClick={handleSaveChatHistory} className="material-symbols-rounded">
-              refresh
-            </button>
-            <button
-              onClick={() => {
-                setShowChatbot(false);
-                setShowScanDropdown(false);
-                setShowChatHistoryDropdown(false);
-              }}
-              className="material-symbols-rounded"
-            >
-              keyboard_arrow_down
-            </button>
+          <div className="inner-header">
+            <div className="header-info">
+              <ChatbotIcon
+                onClick={() => {
+                  setShowChatHistoryDropdown((prev) => !prev);
+                  setShowScanDropdown(false);
+                }}
+              />
+              <h2 className="logo-text">Ozzy</h2>
+            </div>
+            <div className="header-buttons">
+              <button
+                onClick={() => {
+                  setShowScanDropdown((prev) => !prev);
+                  setShowChatHistoryDropdown(false);
+                }}
+                className="material-symbols-rounded"
+              >
+                history
+              </button>
+              <button onClick={handleSaveChatHistory}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+      data-slot="icon"
+      className="bpHeaderContentActionsIcons"
+      role="button"
+      tabIndex="0"
+      style={{ width: "20px", height: "20px" }}
+    >
+      <path
+        fillRule="evenodd"
+        d="M15.312 11.424a5.5 5.5 0 0 1-9.201 2.466l-.312-.311h2.433a.75.75 0 0 0 0-1.5H3.989a.75.75 0 0 0-.75.75v4.242a.75.75 0 0 0 1.5 0v-2.43l.31.31a7 7 0 0 0 11.712-3.138.75.75 0 0 0-1.449-.39Zm1.23-3.723a.75.75 0 0 0 .219-.53V2.929a.75.75 0 0 0-1.5 0V5.36l-.31-.31A7 7 0 0 0 3.239 8.188a.75.75 0 1 0 1.448.389A5.5 5.5 0 0 1 13.89 6.11l.311.31h-2.432a.75.75 0 0 0 0 1.5h4.243a.75.75 0 0 0 .53-.219Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  </button>
+              <button
+                onClick={() => {
+                  setShowChatbot(false);
+                  setShowScanDropdown(false);
+                  setShowChatHistoryDropdown(false);
+                }}
+                className="material-symbols-rounded"
+              >
+                close
+              </button>
+            </div>
           </div>
         </div>
 
